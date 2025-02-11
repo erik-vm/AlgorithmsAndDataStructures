32ecf43c1580b92ee6a24cf2c0199e34dda5d0e3
Erik Vainumäe <erik.vainumae@gmail.com>
2025-02-11 22:12:09 +0200

    hw1 start

---
+++ src/sub1/Sheep.java
@@ -0,0 +1,27 @@
+package sub1;
+
+//An array contains sheep and goats in random order.
+//Write a possibly fast method to rearrange the array,
+//so that all goats are at the beginning and all sheep
+//are at the end of the array.
+//Consider the case when all animals are the same kind.
+//If any sources are used, they must be cited (in form of comments
+//at the beginning of the code).
+//Do not use Java List types (lists are very slow for this task).
+
+
+public class Sheep {
+
+    enum Animal {sheep, goat}
+
+    ;
+
+    public static void main(String[] param) {
+        // for debugging
+    }
+
+    public static void reorder(Animal[] animals) {
+        // TODO!!! Your program here
+    }
+}
+
